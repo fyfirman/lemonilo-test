b7e6e962c7f2a1b068607c96d74ce7e758e7088b
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-16 22:58:39 +0700

    fix: aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        "root": [
+          "./src"
+        ],
+        "alias": {
+          "@assets": "./src/assets",
+          "@constants": "./src/constants",
+          "@components": "./src/components",
+          "@router": "./src/router",
+          "@theme": "./src/theme",
+          "@screens": "./src/screens",
+          "@utils": "./src/utils",
+        },
+      } 
+    ], 
+  ],
 };
