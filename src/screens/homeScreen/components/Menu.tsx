568807696264d4d8829322ec9ed867ec33a7f8c2
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-17 15:25:35 +0700

    refactor: delete menu data

--- src/screens/homeScreen/components/Menu.tsx
+++ src/screens/homeScreen/components/Menu.tsx
@@ -1,7 +1,6 @@
 import arrayToMatrix from '@utils/arrayToMatrix';
 import React from 'react';
 import styled from 'styled-components/native';
-import menuData, { IMenu } from './menu.data';
 import MenuItem from './MenuItem';
 
 const Container = styled.View`
@@ -12,6 +11,24 @@ const MenuContainer = styled.View`
   flex-direction: row;
   justify-content: space-around;
 `;
+
+interface IMenu {
+  label: string;
+  imageUri: string;
+}
+
+const menuData : IMenu[] = [
+  { label: 'Flight', imageUri: 'https://reactnative.dev/img/tiny_logo.png' },
+  { label: 'Hotels', imageUri: 'https://reactnative.dev/img/tiny_logo.png' },
+  { label: 'Flight + Hotel', imageUri: 'https://reactnative.dev/img/tiny_logo.png' },
+  { label: 'Eats', imageUri: 'https://reactnative.dev/img/tiny_logo.png' },
+  { label: 'Trains', imageUri: 'https://reactnative.dev/img/tiny_logo.png' },
+  { label: 'Buses & Shuttles', imageUri: 'https://reactnative.dev/img/tiny_logo.png' },
+  { label: 'Cars', imageUri: 'https://reactnative.dev/img/tiny_logo.png' },
+  { label: 'Motor- cycle', imageUri: 'https://reactnative.dev/img/tiny_logo.png' },
+  { label: 'Finance', imageUri: 'https://reactnative.dev/img/tiny_logo.png' },
+  { label: 'All Products', imageUri: 'https://reactnative.dev/img/tiny_logo.png' },
+];
 
 const Menu = () => {
   const renderMenuItems = () =>
