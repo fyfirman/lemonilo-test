57245c5031b0d9e8538032f5de9f50d6cb0822f6
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-18 07:25:24 +0700

    feat: add icon to bottom tabs (bugged)

--- src/router/HomeTabs.router.tsx
+++ src/router/HomeTabs.router.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Icon from 'react-native-ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '@screens/homeScreen/HomeScreen';
 import SavedScreen from '@screens/savedScreen/SavedScreen';
@@ -10,17 +11,46 @@ import theme from '@theme/theme';
 const Tab = createBottomTabNavigator();
 
 const HomeTabsRouter = () => {
+  const getTabBarIcon = (name: string) => ({
+    color,
+    size,
+  }: {
+    focused: boolean;
+    color: string;
+    size: number;
+  }) => <Icon color={color} size={size} name={name} />;
+
   return (
     <Tab.Navigator
       tabBarOptions={{
         activeTintColor: theme.palette.primary.main,
       }}
     >
-      <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
-      <Tab.Screen name="Saved" component={SavedScreen} options={{ title: 'Saved' }} />
-      <Tab.Screen name="MyBooking" component={MyBookingScreen} options={{ title: 'My Booking' }} />
-      <Tab.Screen name="MyInbox" component={MyInboxScreen} options={{ title: 'My Inbox' }} />
-      <Tab.Screen name="MyAccount" component={MyAccountScreen} options={{ title: 'My Account' }} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Home', tabBarIcon: getTabBarIcon('home') }}
+      />
+      <Tab.Screen
+        name="Saved"
+        component={SavedScreen}
+        options={{ title: 'Saved', tabBarIcon: getTabBarIcon('bookmark') }}
+      />
+      <Tab.Screen
+        name="MyBooking"
+        component={MyBookingScreen}
+        options={{ title: 'My Booking', tabBarIcon: getTabBarIcon('bookmarks') }}
+      />
+      <Tab.Screen
+        name="MyInbox"
+        component={MyInboxScreen}
+        options={{ title: 'My Inbox', tabBarIcon: getTabBarIcon('mail-open') }}
+      />
+      <Tab.Screen
+        name="MyAccount"
+        component={MyAccountScreen}
+        options={{ title: 'My Account', tabBarIcon: getTabBarIcon('contact') }}
+      />
     </Tab.Navigator>
   );
 };
