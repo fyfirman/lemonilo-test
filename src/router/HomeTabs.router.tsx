947d9da57ccfdb9d825f88b7375346f89ecdad5a
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-18 07:06:38 +0700

    fix: lable

--- src/router/HomeTabs.router.tsx
+++ src/router/HomeTabs.router.tsx
@@ -5,17 +5,22 @@ import SavedScreen from '@screens/savedScreen/SavedScreen';
 import MyBookingScreen from '@screens/myBookingScreen/MyBookingScreen';
 import MyInboxScreen from '@screens/myInboxScreen/MyInboxScreen';
 import MyAccountScreen from '@screens/myAccountScreen/MyAccountScreen';
+import theme from '@theme/theme';
 
 const Tab = createBottomTabNavigator();
 
 const HomeTabsRouter = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Saved" component={SavedScreen} />
-      <Tab.Screen name="MyBooking" component={MyBookingScreen} />
-      <Tab.Screen name="MyInbox" component={MyInboxScreen} />
-      <Tab.Screen name="MyAccount" component={MyAccountScreen} />
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: theme.palette.primary.main,
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+      <Tab.Screen name="Saved" component={SavedScreen} options={{ title: 'Saved' }} />
+      <Tab.Screen name="MyBooking" component={MyBookingScreen} options={{ title: 'My Booking' }} />
+      <Tab.Screen name="MyInbox" component={MyInboxScreen} options={{ title: 'My Inbox' }} />
+      <Tab.Screen name="MyAccount" component={MyAccountScreen} options={{ title: 'My Account' }} />
     </Tab.Navigator>
   );
 };
