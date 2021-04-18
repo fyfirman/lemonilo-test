5d8efdfe309cfd3975493a6969a56732cf244245
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-18 15:14:22 +0700

    feat: add helper and snapshot

--- src/__tests__/helpers.tsx
+++ src/__tests__/helpers.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import theme from '@theme/theme';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 
 export const renderWithTheme = (component: React.ReactNode) =>
   renderer.create(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
