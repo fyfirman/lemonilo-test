f8a02e487fdd248a7d76fc0c5bd17b9603fadb25
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-18 15:15:11 +0700

    refactor

--- src/components/Avatar/Avatar.test.tsx
+++ src/components/Avatar/Avatar.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { renderWithTheme } from '../../__tests__/helpers';
-import Avatar from '../Avatar';
+import Avatar from './Avatar';
 import 'jest-styled-components';
 
 const image = require('@assets/images/avataaars.png');
--- src/screens/homeScreen/components/AccountInfo.tsx
+++ src/screens/homeScreen/components/AccountInfo.tsx
@@ -1,4 +1,4 @@
-import Avatar from '@components/Avatar';
+import Avatar from '@components/Avatar/Avatar';
 import React from 'react';
 import styled from 'styled-components/native';
 import AccountAction from './AccountAction';
